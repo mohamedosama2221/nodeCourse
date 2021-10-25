ef3ca41b40edd592fcef6620a6af3dc00ad9a40e
Eng-Mohamed-Osama <eng.muhamed.osama@gmail.com>
2021-10-25 09:50:48 +0400

    globals in node js

---
+++ 2-globals.js
@@ -0,0 +1,8 @@
+//Globals
+//__dirname - path to the current directory
+//__filename - file name
+//require -function to use modules
+//module  -info about current module
+//process - info about env where the program is being executed
+
+console.log(process);
