cadbaa356b0364856bd2a6a0e199bf4942c1fc21
Eng-Mohamed-Osama <eng.muhamed.osama@gmail.com>
2021-10-28 17:44:28 +0400

    express router

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,43 +1,17 @@
-//app.get //read data
-//app.post //insert data
-//app.delete //delete data
-//app.put //update data
-//app.all
-//app.listen
-//app.use
-
 const express = require("express");
 const app = express();
-let { people } = require("./data");
+const peopleRouter = require("./router/people");
+const authRouter = require("./router/auth");
+
+app.use(express.static("./methods-public"));
 
 app.use(express.json());
 
-// app.get("/people/:id", (req, res) => {
-//   return res.status(200).json({ success: true, data: { people } });
-// });
-app.put("/people/:id", (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-  const person = people.find((person) => person.id === Number(id));
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `there is no person with that ${id}` });
-  }
-  const newPerson = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name;
-    }
-    return person;
-  });
-  return res.status(200).json({ success: true, data: newPerson });
-});
+app.use(express.urlencoded({ extended: false }));
 
-app.delete("/people/:id", (req, res) => {
-  const { id } = req.params;
-  const newPeople = people.filter((p) => p.id !== Number(id));
-  return res.status(200).json({ success: true, data: newPeople });
-});
+app.use("/api/people", peopleRouter);
+
+app.use("/login", authRouter);
 
 app.listen(5000, () => {
   console.log("listening");
