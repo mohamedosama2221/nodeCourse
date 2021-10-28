8b8ff57ef7be778351854445223ef978ad34ac52
Eng-Mohamed-Osama <eng.muhamed.osama@gmail.com>
2021-10-28 17:06:33 +0400

    put and delete methods

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -5,64 +5,38 @@
 //app.all
 //app.listen
 //app.use
+
 const express = require("express");
-const { products } = require("./data");
-const { logger } = require("./middleware/logger");
-const { auth } = require("./middleware/auth");
-
 const app = express();
-// app.use(express.static("./public"));
-app.use([logger, auth]); //use middleware if it was placed before another route then it wont get excuted thats why the app.use always start first
-//can also use a path as first argument to specifily applay the middleware to certain routes
-//in this example it will apply to any route that start with (/products) and anything after it will work
-//using muliple middleware with an array  //the order of the middleware in the array will determine the order of excusions
+let { people } = require("./data");
 
-app.get("/", (req, res) => {
-  return res
-    .status(200)
-    .send(
-      `<h1>welcome back ${req.user.name}</h1><a href="http://localhost:5000/products">products</a>`
-    );
-});
+app.use(express.json());
 
-app.get("/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
+// app.get("/people/:id", (req, res) => {
+//   return res.status(200).json({ success: true, data: { people } });
+// });
+app.put("/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `there is no person with that ${id}` });
+  }
+  const newPerson = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
   });
-  return res.status(200).json({ newProducts });
+  return res.status(200).json({ success: true, data: newPerson });
 });
 
-app.get("/products/:id", (req, res) => {
-  const { id } = req.params; //route paramter
-  const singleProducts = products.filter(
-    (product) => product.id === parseInt(id)
-  );
-  if (singleProducts.length === 0) {
-    return res.status(404).send("<h1>this product not found </h1>");
-  }
-  return res.status(200).json({ singleProducts });
-});
-
-app.get("/v1/query", (req, res) => {
-  const { search, limit } = req.query; //query params
-  let sortedProducts = [...products];
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) =>
-      product.name.includes(search)
-    );
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (!sortedProducts.length) {
-    return res.status(200).json({ sucess: true, data: [] });
-  }
-  return res.status(200).json({ sortedProducts });
-});
-
-app.all("*", (req, res) => {
-  return res.status(404).send("<h1>Oops not found!</h1>");
+app.delete("/people/:id", (req, res) => {
+  const { id } = req.params;
+  const newPeople = people.filter((p) => p.id !== Number(id));
+  return res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(5000, () => {
